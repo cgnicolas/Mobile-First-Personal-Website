177cefb340537f87c40406736252de0af9216315
cgnicolas <cgnicolas@gmail.com>
2019-10-07 22:49:33 -0400

    Added experience switcher

---
+++ src/Components/ExperienceSwitcher.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class ExperienceSwitcher extends Component {
+    state = { 
+        formal: this.props.formal,
+     }
+    render() {
+        let {changeExperience} = this.props;
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default ExperienceSwitcher;
--- src/Containers/Experience.jsx
+++ src/Containers/Experience.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import ExperienceCard from '../Components/ExperienceCard';
-
+import ExperienceSwitcher from '../Components/ExperienceSwitcher';
 class Experience extends Component {
-    state = {  }
+    state = { 
+        formal: true
+     }
+    
+     constructor(props){
+         super(props);
+
+         this.changeExperience = this.changeExperience.bind(this);
+
+     }
+
+    changeExperience() {
+        this.setState({
+            formal: !this.state.formal,
+        })
+    }
+
     render() { 
         let {data} = this.props.content;
         return ( 
             <section className="experience-section grid">
                 <div className="content-wrap">
                     <h1 className="section-title">experience</h1>
-                    {/* Experience Switcher */}
+                    <ExperienceSwitcher formal={this.state.formal} changeExperience={() => {this.changeExperience()}}/>
                     {data.map((element, index) => {
                         return (
                             <ExperienceCard key={index} content={element}/>
