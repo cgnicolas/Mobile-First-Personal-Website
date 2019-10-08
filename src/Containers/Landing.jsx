a21020b3b794510080c2094adc110e8b315d9cd9
cgnicolas <cgnicolas1@gmail.com>
2019-10-08 16:40:34 -0400

    Added background for landing page

--- src/Containers/Landing.jsx
+++ src/Containers/Landing.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ContactButton from '../Components/ContactButton';
+import landingBackground from '../Assets/Content/Images/LandingBackground.svg';
 class Landing extends Component {
     state = {  }
     render() { 
         return ( 
             <section className="landing-section">
                 <div className="landing-content">
+                    <img src={landingBackground} alt="background_image"/>
                     <h1>Hi there! I'm Cedric.</h1>
                     <h2>I'm a Software Developer.</h2>
                     {/* <button name="Get In Touch" type="button" className="contactButton"></button> */}
