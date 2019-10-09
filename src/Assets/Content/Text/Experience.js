08d498aca7efd08204fec810259d105b881f28d4
cgnicolas <cgnicolas@gmail.com>
2019-10-08 23:35:51 -0400

    Added complete formal experience content, sizes are still not fixed

--- src/Assets/Content/Text/Experience.js
+++ src/Assets/Content/Text/Experience.js
@@ -2,7 +2,8 @@ export default {
     data: [
         {
             title: "American Express",
-            descriptions: ["Utilized AGILE methodologies along with JIRA to develop an end-to-end marketing offer flow for current card offers within an uplifted dashboard using React", 
+            descriptions: [
+                "Utilized AGILE methodologies along with JIRA to develop an end-to-end marketing offer flow for current card offers within an uplifted dashboard using React", 
             "Added functionality to a feature in the application to be used by over 6000 customer service representatives",
             "Applied concepts of REST and UI/UX to build a fully functional and interactive offer flow utilizing SpringBoot and Java",
             "Participated in a nationwide Intern Hackathon to build a mobile food ordering application using React Native"
@@ -10,11 +11,17 @@ export default {
         },
         {
             title: "Design For X Lab",
-            descriptions: ["Lorem Ipsum", "Lorem Ipsum", "Lorem Ipsum"]
+            descriptions: [
+                "Developed a Tool Checkout system for student and assistant use within the lab using React, Node, and Postgres"
+            ]
         },
         {
             title: "Citi Bank",
-            descriptions: ["Lorem Ipsum", "Lorem Ipsum", "Lorem Ipsum"]
+            descriptions: [
+                "Improved upon existing Java Web Application built with SmartGWT", 
+                "Utilized Spring and SQL in order to add dynamic data population to a newly created webpage", 
+                "Assisted in automating the deletion of certain records based on business day calculation"
+            ]
         },
     ]
 }
