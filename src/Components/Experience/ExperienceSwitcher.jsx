81e0f29f6bbaaaf5a4788e53ef30c235cfcd0dfe
cgnicolas <cgnicolas1@gmail.com>
2019-10-10 14:52:47 -0400

    Experience switcher buttons working

--- src/Components/Experience/ExperienceSwitcher.jsx
+++ src/Components/Experience/ExperienceSwitcher.jsx
@@ -3,16 +3,18 @@ import React, { Component } from 'react';
 
 class ExperienceSwitcher extends Component {
     state = { 
-        formal: this.props.formal,
-     }
+        // formal: this.props.formal,
+    }
     render() {
         let {changeExperience} = this.props;
-        let {formal} = this.state;
+        let {formal} = this.props;
+        console.log(formal);
         return (
             <div className="experience-switcher">
                 <div className="experience-switcher-content-wrap">
-                    <button className={"formal-button " + ((formal) ? ("active-button") : (""))} onClick={() => changeExperience(true)}>Formal</button>
-                    <button className={"formal-button " + ((!formal) ? ("active-button") : (""))} onClick={() => changeExperience(false)}>Informal</button>
+                    <div className=""></div>
+                    <button className={"formal-button " + ((formal) ? ("active-button") : ("inactive"))} onClick={() => changeExperience(true)}>Formal</button>
+                    <button className={"formal-button " + ((formal) ? ("inactive") : ("active-button"))} onClick={() => changeExperience(false)}>Informal</button>
                 </div>
             </div>
         );
