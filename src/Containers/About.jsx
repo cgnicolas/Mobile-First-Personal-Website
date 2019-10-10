413f1069a80221fc9c02b3266150061a1fb83f8d
cgnicolas <cgnicolas1@gmail.com>
2019-10-10 13:24:16 -0400

    About section somewhat responsive

--- src/Containers/About.jsx
+++ src/Containers/About.jsx
@@ -5,24 +5,28 @@ class About extends Component {
     render() { 
         let {content, technologies} = this.props;
         return ( 
-            <section className="about-section grid">
+            <section className="about-section">
                 <div className="content-wrap">
                     <h1 className="section-title">about</h1>
-                    <div className="content-card">
-                        <img src={selfPhoto} className='selfPhoto' alt="photo_of_me"/>
-                        {content.map((element, index) => {
-                            return (
-                                <p key={index} className="text-content">{element}</p>
-                            )
-                        })}
-                        <div className="technologies">
-                            <ul>
-                                {technologies.map((technology, index) => {
-                                    return (
-                                        <li key={index}><p>{technology}</p></li>
-                                    )
-                                })}
-                            </ul>
+                    <div className="content-card grid">
+                        <div className="photo-wrapper">
+                            <img src={selfPhoto} className='selfPhoto' alt="photo_of_me"/>
+                        </div>
+                        <div className="about-content">
+                            {content.map((element, index) => {
+                                return (
+                                    <p key={index} className="text-content">{element}</p>
+                                )
+                            })}
+                            <div className="technologies">
+                                <ul>
+                                    {technologies.map((technology, index) => {
+                                        return (
+                                            <li key={index}><p>{technology}</p></li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
